9dc9b9eb3a827b6e8054682d2efba5931bd7e5d2
WilgotW <wilgotwardell@hotmail.com>
2026-02-15 17:43:46 +0100

    -

---
+++ laboration4/LinkedListQ.py
@@ -0,0 +1,110 @@
+class LinkedListQ:
+    def __init__(self):
+        self.__head = None #the first item in the queue
+        self.__tail = None #the last item in the queue
+
+    def __len__(self):
+        length = 0
+        
+        temp = self.__head
+        while temp != self.__tail:
+            length += 1
+            temp = temp.next
+        
+        return length
+
+    def __str__(self):
+        if self.__head == None:
+            return "No data"
+
+        que_string = str(self.__head)
+
+        temp = self.__head
+        while temp != self.__tail:
+            temp = temp.next
+            que_string += str(temp)
+
+        return que_string
+
+    def __contains__(self, key):
+        temp = self.__head
+
+        if temp is None:
+            return False
+        
+        if temp.data == key:
+            return True
+        while temp != self.__tail:
+            if temp.data == key:
+                return True
+            temp = temp.next
+        return False
+
+    #This algorithm is O(1). Besides remove(x)
+    def enqueue(self, data):
+        new_node = Node(data)
+
+        if self.__tail != None:
+            #point current tail to new tail. 
+            self.__tail.next = new_node
+            self.__tail = new_node
+        else:
+            #queue is empty
+            self.__head = new_node
+            self.__tail = new_node
+
+    def dequeue(self):
+        if self.__head == None:
+            return None
+
+        return_data = self.__head.data
+        self.__head = self.__head.next
+
+        if self.__head == None:
+            self.__tail = None
+
+        return return_data
+
+    def isEmpty(self):
+        if self.__head == None:
+            return True
+        return False
+
+    def remove(self, data):
+
+        if self.__head is None:
+            return
+
+        if self.__head.data == data:
+            self.__head = self.__head.next
+
+            if self.__head is None:
+                self.__tail = None
+
+            return
+
+        previous = self.__head
+        current = self.__head.next
+
+        while current != None:
+            if current.data == data:
+                #unlink the wanted node
+                previous.next = current.next
+
+                #if we unlinked the tail
+                if current == self.__tail:
+                    self.__tail = previous
+
+                return
+            #move pointers forward
+            previous = current
+            current = current.next
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __str__(self):
+        return str(self.data)
+
--- laboration4/main.py
+++ laboration4/main.py
@@ -1,9 +1,15 @@
 from bintree import Bintree
+from LinkedListQ import LinkedListQ
 from helpers import *
 
 alphabet = "abcdefghijklmnopqrstuvwxyzåäö"
 
-def make_children(word, word_tree, visited_words_tree:Bintree, print_ = False):
+class ParentNode:
+    def __init__(self, word, parent = None):
+        self.word = word
+        self.parent = parent
+        
+def make_children(word, word_tree, visited_words_tree:Bintree, queue_of_words:LinkedListQ, print_ = False):
     word_combinations = []
     _alphabet = str_to_letters(alphabet)
 
@@ -21,6 +27,7 @@ def make_children(word, word_tree, visited_words_tree:Bintree, print_ = False):
                 and new_word != word
                 and new_word in word_tree
                 and new_word not in visited_words_tree
+                and new_word not in queue_of_words
                 ):
                 word_combinations.append(new_word)
     
@@ -38,28 +45,52 @@ def load_words():
             word_tree.store(word)
     return word_tree
 
-# Ett av barnen till söt är söm och motsvarande är ett av barnen till söm är söt.
-
-# Om du inte tar hänsyn till redan besökta ord så kan du få oändliga kedjor och aldrig komma fram till någon lösning.
-
-# söt -> söm -> söt -> söm -> söt -> söm -> söt ...
-# söt -> söm -> döm -> dum -> döm -> söm -> söt ...
-# Spara därför undan redan besökta ord i ytterligare ett binärträd och modifiera makechildren så att enbart barn som inte förekommit tidigare genereras.
-
-# Skapa minst ett fördefinierat testfall där du testar att det redan-besökta-trädet fungerar. Om du exempelvis lägger in ordet söm så borde söt få 9 unika barn istället för 10.
-
 def main():
     word_tree = load_words()
     visited_words_tree = Bintree()
 
+    queue_of_words = LinkedListQ()
+    target_word = "söt"
+
+    starting_word = "sur"
+    queue_of_words.enqueue(starting_word)
+
+    word_found = bfs(queue_of_words, target_word, word_tree, visited_words_tree)
+    print(word_found)
+
+#1. Lägg till första ordet i kön. 
+#2. Kolla ordet: är det de sökta ordet? Om ja returnera
+#3. Hitta barnen med make_children och lägg till alla barn i kön
+#4. gå till första barnet -> rätt ord? returna. fel ord? lägg till alla deras barn i kön
+#5. fortsätt tills rätta ordet hittas 
+def bfs(queue_of_words:LinkedListQ, target_word, word_tree, visited_words_tree):
+    current_word = queue_of_words.dequeue()
+
+    print(queue_of_words)
+    input()
+
+    if current_word == target_word: 
+        return True
+
+    children = make_children(current_word, word_tree, visited_words_tree, queue_of_words)
+    for child in children:
+        queue_of_words.enqueue(child)
+    
+    if len(queue_of_words) > 0:
+        return bfs(queue_of_words, target_word, word_tree, visited_words_tree)
+    
+    return False
+    
+
+
+
+    
+def test(word_tree, visited_words_tree):
     make_children("söt", word_tree, visited_words_tree, True)
     print("---")
     make_children("söm", word_tree, visited_words_tree, True)
     print("---")
 
     print()
-
-    
-
 
 main()
