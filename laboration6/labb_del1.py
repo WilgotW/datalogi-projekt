aa56834f61e726bd37ebf02c71bcb658e400d755
WilgotW <wilgotwardell@hotmail.com>
2026-06-07 18:25:06 +0200

    -

--- laboration6/labb_del1.py
+++ laboration6/labb_del1.py
@@ -44,7 +44,6 @@ def average_random_linear_search(song_list, iterations=1000):
         linear_search(song_list, target.artist)
 
 def test_queue(q, lst):
-    """Lägger in alla element i kön och plockar därefter ut dem"""
     for item in lst:
         q.enqueue(item) 
     
@@ -52,15 +51,12 @@ def test_queue(q, lst):
         q.dequeue()
 
 def test_bintree(tree, lst):
-    """Lägger in alla element i binärträdet"""
     for item in lst:
         tree.store(item.artist)
         
 def test_hashtable(ht, lst):
-    """Lägger in alla element i hashtabellen"""
     for item in lst:
-        ht.store(item.artist, item) # Byt till ht.put() beroende på din labb 5-implementation
-
+        ht.store(item.artist, item) 
 
 
 def main():
@@ -74,50 +70,50 @@ def main():
         fake_artist = "fake_artist"
         fake_song = Song("", "", fake_artist, "")
         
-        #1.Linjärsök osorterad, kommer söka hela listan, 
+        #1.Linjärsök osorterad, kommer söka hela listan, O(n)
         t_lin_osort = timeit.timeit(stmt=lambda: linear_search(test_list, fake_song), number=1)
         print(f"I - Linjärsök osorterad: {t_lin_osort:.5f} sekunder")
         
-        #3. Linjärsökning efter 1000 slumpvalda element
+        #3. Linjärsökning efter 1000 slumpvalda element, O(n)
         t_lin_rand = timeit.timeit(stmt=lambda: average_random_linear_search(test_list, 1000), number=1)
         print(f"III - Linjärsök (snitt 1000): {(t_lin_rand / 1000):.5f} sek/sökning (Total: {t_lin_rand:.2f}s)")
 
-        #4.sortera med merge sort 
+        #4. sortera med merge sort, O(n log n)
         t_sort = timeit.timeit(stmt=lambda: merge_sort(test_list.copy()), number=1)
         print(f"IV - Mergesort: {t_sort:.5f} sekunder")
 
-        #skapa sorterade listan
+        # skapa sorterade listan, O(n log n)
         sorted_list = merge_sort(test_list.copy())
 
-        #2. Linjärsökning i sorterad lista
+        #2. Linjärsökning i sorterad lista, O(n)
         t_lin_sort = timeit.timeit(stmt=lambda: linear_search(sorted_list, fake_song), number=1)
         print(f"II - Linjärsök sorterad: {t_lin_sort:.5f} sekunder")
 
-        #5. Binärsökning i sorterad lista
+        #5. Binärsökning i sorterad lista, O(log n)
         t_bin_sort = timeit.timeit(stmt=lambda: binary_search(sorted_list, fake_song), number=1)
         print(f"V - Binärsök sorterad: {t_bin_sort:.5f} sekunder")
         
-        #6. IndexListQ, lägg in element med kö. 
+        #6. IndexListQ, lägg in element med kö, O(n^2)
         idx_q = IndexListQ()
         t_idxq = timeit.timeit(stmt=lambda: test_queue(idx_q, test_list), number=1)
         print(f"VI - IndexListQ: {t_idxq:.5f} sekunder")
 
-        #7. LinkedListQ
+        #7. LinkedListQ, O(n)
         ll_q = LinkedListQ()
         t_llq = timeit.timeit(stmt=lambda: test_queue(ll_q, test_list), number=1)
         print(f"VII - LinkedListQ: {t_llq:.5f} sekunder")
 
-        #8. Binärträd
+        #8. Binärträd, O(n log n)
         tree = Bintree()
         t_tree = timeit.timeit(stmt=lambda: test_bintree(tree, test_list), number=1)
         print(f"VIII - Binärträd: {t_tree:.5f} sekunder")
 
-        #9. Hashtabell n*2
+        #9. Hashtabell n*2, O(n)
         ht2 = Hashtable(n * 2)
         t_ht2 = timeit.timeit(stmt=lambda: test_hashtable(ht2, test_list), number=1)
         print(f"IX - Hashtabell (n*2): {t_ht2:.5f} sekunder")
 
-        #10. Hashtabell n*1.5
+        #10. Hashtabell n*1.5, O(n)
         ht15 = Hashtable(int(n * 1.5))
         t_ht15 = timeit.timeit(stmt=lambda: test_hashtable(ht15, test_list), number=1)
         print(f"X - Hashtabell (n*1.5): {t_ht15:.5f} sekunder")
